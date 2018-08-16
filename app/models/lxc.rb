38cc15476d5f2dd2259d0b3029797e996fdd2e58
reeechart <ferdinandusrichard@yahoo.co.id>
2018-08-16 14:31:03 +0700

    [farid/reeechart] dashboard able to show lxc creation failures

--- app/models/lxc.rb
+++ app/models/lxc.rb
@@ -1,7 +1,7 @@
 class Lxc
-  attr_reader :id, :name, :ip_address, :image, :status, :lxd_id, :host_port, :container_port
+  attr_reader :id, :name, :ip_address, :image, :status, :lxd_id, :host_port, :container_port, :error_message
 
-  def initialize(id, name, ip_address, image, status, lxd_id, host_port, container_port)
+  def initialize(id, name, ip_address, image, status, lxd_id, host_port, container_port, error_message)
     @id = id
     @name = name
     @ip_address = ip_address
@@ -10,5 +10,6 @@ class Lxc
     @lxd_id = lxd_id
     @host_port = host_port
     @container_port = container_port
+    @error_message = error_message
   end
 end
